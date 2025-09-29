96e2079ba7ec5492a5bebadfe1002de148f053d7
Bane Dragic <bane.itmedia@gmail.com>
2025-09-29 22:16:12 +0200

    FEAT: Step interface and hard-coded steps values

--- components/stepper/steps.ts
+++ components/stepper/steps.ts
@@ -1,5 +1,11 @@
-interface Step {
-	title: string;
+export interface Step {
+    id: number;
+    title: string;
 }
 
-export const steps: Step[] = [];
+export const steps: Step[] = [
+    {id: 1, title: "Service"},
+    {id: 2, title: "Termin"},
+    {id: 3, title: "Fahrzeug"},
+    {id: 4, title: "Kontakt"},
+];

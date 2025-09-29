2427fd6c38399d8eeda1ddd8b35c9b77605abaf1
Bane Dragic <bane.itmedia@gmail.com>
2025-09-29 22:38:51 +0200

    FEAT: Stepper shared hook

--- hooks/useStepper.ts
+++ hooks/useStepper.ts
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import {Step} from '../components/stepper/steps';
 
-export default function useStepper() {
-	const [currentStep, setCurrentStep] = useState<number>(0);
-	function handleNextStep() {
-		setCurrentStep((prev) => {
-			return prev + 1;
-		});
-	}
-	return { currentStep, handleNextStep };
+export function useStepper(steps: Step[]) {
+	const [currentStep, setCurrentStep] = useState(0);
+
+	const handleNextStep = () => {
+		setCurrentStep((prev) => Math.min(prev + 1, steps.length - 1));
+	};
+
+	return {
+		currentStep,
+		handleNextStep,
+		isLastStep: currentStep === steps.length - 1,
+	};
 }

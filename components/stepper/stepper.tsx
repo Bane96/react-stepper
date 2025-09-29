d6d10faef21a3274332d688d7bf57283f2cb1600
Bane Dragic <bane.itmedia@gmail.com>
2025-09-29 23:58:38 +0200

    FIX: Delete unnecessary condition for connector line

--- components/stepper/stepper.tsx
+++ components/stepper/stepper.tsx
@@ -1,38 +1,36 @@
 import type {Step} from "./steps";
 import {memo} from 'react';
+
 interface StepperProps {
-	steps: Step[];
-	currentStep: number;
+    steps: Step[];
+    currentStep: number;
 }
 
 function Stepper({steps, currentStep}: StepperProps) {
-	return (
-		<>
-			<ol className="stepper" >
-				{steps.map((step, index) => {
-					const stepNumber = index + 1;
-					const isActive = currentStep >= index;
-					const isConnectorActive = currentStep > index;
+    return (
+        <>
+            <ol className="stepper">
+                {steps.map((step, index) => {
+                    const stepNumber = index + 1;
+                    const isActive = currentStep >= index;
 
-					return (
-						<li
-							key={step.id}
-							className={`step-item ${isActive ? "active" : ""} ${
-								isConnectorActive ? "active-connector" : ""
-							}`}
-						>
-							<div className="block whitespace-nowrap z-10">
-								<span className="step">{stepNumber}</span>
-								<p className="step-text">
-									{step.title}
-								</p>
-							</div>
-						</li>
-					);
-				})}
-			</ol>
-		</>
-	);
+                    return (
+                        <li
+                            key={step.id}
+                            className={`step-item ${isActive ? "active" : ""}`}
+                        >
+                            <div className="block whitespace-nowrap z-10">
+                                <span className="step">{stepNumber}</span>
+                                <p className="step-text">
+                                    {step.title}
+                                </p>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ol>
+        </>
+    );
 }
 
 export default memo(Stepper);

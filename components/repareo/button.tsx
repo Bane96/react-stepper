793b6b1f5809e37a0bc7293ed2e2421d27edfb67
Bane Dragic <bane.itmedia@gmail.com>
2025-09-29 22:47:42 +0200

    FEAT: Implement logic to hide button on last step

--- components/repareo/button.tsx
+++ components/repareo/button.tsx
@@ -1,26 +1,28 @@
-import type { ButtonHTMLAttributes, ReactNode } from "react";
+import type {ButtonHTMLAttributes, ReactNode} from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-	children: ReactNode;
-	color?: "primary" | "secondary";
+    children: ReactNode;
+    color?: "primary" | "secondary";
+    isHidden?: boolean;
 }
 
 export default function Button({
-	onClick,
-	children,
-	color = "primary",
-}: ButtonProps) {
-	let colorCns = "bg-blue-600 text-white";
-	if (color === "secondary") {
-		colorCns = "bg-white border border-gray-200 text-black";
-	}
-	return (
-		<button
-			onClick={onClick}
-			type={"button"}
-			className={`${colorCns} hover:opacity-90 shadow-sm py-3 px-6 w-full rounded-lg lg:mt-8 mt-6`}
-		>
-			{children}
-		</button>
-	);
+                                   onClick,
+                                   children,
+                                   color = "primary",
+                                   isHidden
+                               }: ButtonProps) {
+    let colorCns = "bg-blue-600 text-white";
+    if (color === "secondary") {
+        colorCns = "bg-white border border-gray-200 text-black";
+    }
+    return (
+        <button
+            onClick={onClick}
+            type={"button"}
+            className={`${colorCns} ${isHidden ? 'hidden' : ''} hover:opacity-90 shadow-sm py-3 px-6 w-full rounded-lg lg:mt-8 mt-6`}
+        >
+            {children}
+        </button>
+    );
 }

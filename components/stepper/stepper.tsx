a0979c6242f4b0e3cd7a1a54b6a6d6b1542e0f78
Bane Dragic <bane.itmedia@gmail.com>
2025-09-29 22:36:47 +0200

    FEAT: Stepper component layout and all logic

--- components/stepper/stepper.tsx
+++ components/stepper/stepper.tsx
@@ -1,8 +1,38 @@
-interface StepperProps {}
+import type {Step} from "./steps";
+import {memo} from 'react';
+interface StepperProps {
+	steps: Step[];
+	currentStep: number;
+}
 
-export default function Stepper() {
+function Stepper({steps, currentStep}: StepperProps) {
 	return (
 		<>
+			<ol className="stepper" >
+				{steps.map((step, index) => {
+					const stepNumber = index + 1;
+					const isActive = currentStep >= index;
+					const isConnectorActive = currentStep > index;
+
+					return (
+						<li
+							key={step.id}
+							className={`step-item ${isActive ? "active" : ""} ${
+								isConnectorActive ? "active-connector" : ""
+							}`}
+						>
+							<div className="block whitespace-nowrap z-10">
+								<span className="step">{stepNumber}</span>
+								<p className="step-text">
+									{step.title}
+								</p>
+							</div>
+						</li>
+					);
+				})}
+			</ol>
 		</>
 	);
 }
+
+export default memo(Stepper);
